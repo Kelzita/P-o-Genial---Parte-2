33713c405c202cae58e5ddb085152765d1fc2af9
raquelfernandes <raquelfernandesbriyoidjdj@gmail.com>
2025-07-18 13:25:50 -0300

    create banco sql

---
+++ banco de dados/Banco.sql
@@ -0,0 +1,147 @@
+/*Criando um Banco de Dados para a Padaria*/
+CREATE DATABASE Padaria_Pao_Genial;
+USE Padaria_Pao_Genial;
+/*Tabela Função*/
+CREATE TABLE funcao (
+ id_funcao int not null,
+ nome_funcao varchar(50),
+ PRIMARY KEY(id_funcao)
+);
+/*Tabela Usuário*/
+CREATE TABLE usuario(
+id_usuario int not null AUTO_INCREMENT,
+ nome_usuario varchar(50),
+ senha varchar(20),
+ PRIMARY KEY(id_usuario)
+);
+/*Tabela Funcionários*/
+CREATE TABLE funcionarios (
+ id_funcionario INT not null AUTO_INCREMENT,
+ id_funcao int not null,
+ id_usuario int not null,
+ nome_funcionario varchar(80),
+ CPF varchar(20),
+ email varchar(80),
+ cep varchar(15),
+ rua varchar(30),
+ telefone varchar(20),
+ numero int,
+ bairro varchar(80),
+ cidade varchar(20),
+ UF char(2),
+ data_admissao date,
+ salario decimal(10,2),
+ PRIMARY KEY(id_funcionario),
+ FOREIGN KEY (id_funcao) REFERENCES funcao(id_funcao),
+ FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
+
+);
+/*Tabela Fornecedores*/
+CREATE TABLE fornecedores (
+ id_fornecedor int not null AUTO_INCREMENT,
+ nome_fornecedor varchar(80),
+ CNPJ varchar(15),
+ email varchar(80),
+ telefone varchar(20),
+ cep varchar(15),
+ rua varchar(30),
+ numero int,
+ bairro varchar(80),
+ cidade varchar(20),
+ UF char(2),
+
+ PRIMARY KEY(id_fornecedor)
+);
+/*Tabela Estoque*/
+CREATE TABLE estoque (
+ id_estoque int not null AUTO_INCREMENT,
+ qtd_atual int,
+ qtd_min int,
+ qtd_max int,
+
+ PRIMARY KEY(id_estoque)
+);
+/*Tabela Produto*/
+CREATE TABLE produto (
+ id_produto int not null AUTO_INCREMENT,
+ id_fornecedor int not null,
+ id_estoque int not null,
+ nome_produto varchar(80),
+ preco decimal(10,2),
+ unmedida char(10),
+ validade date,
+
+ PRIMARY KEY(id_produto),
+ FOREIGN KEY(id_fornecedor) REFERENCES fornecedores(id_fornecedor),
+ FOREIGN KEY(id_estoque) REFERENCES estoque(id_estoque)
+);
+/*Tabela Caixa*/
+CREATE TABLE caixa (
+ id_caixa int not null AUTO_INCREMENT,
+ id_funcionario int not null,
+ data_abertura date,
+ data_fechamento date,
+ hora_abertura time,
+ hora_fechamento time,
+ valor_inicial decimal(10,2),
+ valor_final decimal(10,2),
+
+ PRIMARY KEY(id_caixa),
+ FOREIGN KEY(id_funcionario) REFERENCES funcionarios(id_funcionario)
+);
+/*Tabela Forma de Pagamento*/
+CREATE TABLE forma_pagamento (
+ id_forma_pag int not null AUTO_INCREMENT,
+ descricao varchar(50),
+
+ PRIMARY KEY(id_forma_pag)
+);
+/*Tabela Comanda*/
+CREATE TABLE comanda (
+ id_comanda int not null AUTO_INCREMENT,
+ id_funcionario int not null,
+ data_abertura date,
+ hora_abetura time,
+ status varchar(10),
+ qtd int,
+
+ PRIMARY KEY(id_comanda),
+ FOREIGN KEY(id_funcionario) REFERENCES funcionarios(id_funcionario)
+);
+/*Tabela Venda*/
+CREATE TABLE venda (
+ id_venda int not null AUTO_INCREMENT,
+ id_caixa int not null,
+ id_forma_pag int not null,
+ data_dia date,
+ hora time,
+ total decimal(10,2),
+
+ PRIMARY KEY(id_venda),
+ FOREIGN KEY(id_caixa) REFERENCES caixa(id_caixa),
+ FOREIGN KEY(id_forma_pag) REFERENCES forma_pagamento(id_forma_pag)
+);
+/*Tabela ItemVenda*/
+CREATE TABLE itemvenda (
+ id_item int not null AUTO_INCREMENT,
+ id_produto int not null,
+ id_venda int not null,
+ preco_un decimal(10,2),
+ qtd int,
+
+ PRIMARY KEY(id_item),
+ FOREIGN KEY(id_produto) REFERENCES produto(id_produto),
+ FOREIGN KEY(id_venda) REFERENCES venda(id_venda)
+);
+
+/*Tabela item comanda*/
+CREATE TABLE item_comanda (
+ id_item_comanda INT NOT NULL AUTO_INCREMENT,
+ id_comanda INT NOT NULL,
+ id_produto INT NOT NULL,
+ qtd INT,
+ observacao VARCHAR(100),
+ PRIMARY KEY(id_item_comanda),
+ FOREIGN KEY(id_comanda) REFERENCES comanda(id_comanda),
+ FOREIGN KEY(id_produto) REFERENCES produto(id_produto)
+);
